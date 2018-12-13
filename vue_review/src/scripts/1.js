e4193a7a0b61a33b200db18bb6cef83832f9337d
wlc <1272071495@qq.com>
2018-12-13 09:28:31 +0800

    12-13

---
+++ vue_review/src/scripts/1.js
@@ -0,0 +1,18 @@
+/**
+ *
+ *   第一种
+ * 将光标放置于function上面，快捷键是 Ctrl+Alt+D 加 Ctrl+Alt+D。
+ *
+ * @param {*} a
+ * @param {*} b
+ */
+function test(a, b) {}
+
+/**
+ * 在函数名后添加/**后,回车
+ *
+ *
+ * @param {*} a
+ * @param {*} b
+ */
+function ba(a, b) {}
